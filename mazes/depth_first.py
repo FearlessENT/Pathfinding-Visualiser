dec47abec78541dc346b8f8c102e0f9f3cdf0009
FearlessENT <anthony_fear@rocketmail.com>
2020-02-06 12:45:00 +0000

    adding depth first

--- mazes/depth_first.py
+++ mazes/depth_first.py
@@ -9,3 +9,32 @@ import grid_utils
 
 def generate_maze(grid):
     print("ss")
+
+    grid_utils.clear_grid(grid)
+    grid_utils.fill_grid(grid)
+
+
+    starting_node = get_random_node(grid)
+
+
+
+    starting_node.turn_to_free()
+
+
+
+
+
+
+
+
+def get_random_node(grid):
+
+    width = len(grid[1])
+    height = len(grid)
+
+    randomx = random.randint(2, int(width / 2))
+    randomy = random.randint(2, int(height / 2))
+
+    chosen_cell = grid[randomx][randomy]
+
+    return chosen_cell
